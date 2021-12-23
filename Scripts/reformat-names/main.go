02f1a4a00f213a7f84534519bea2ac11fe1f8db4
kamaal111 <kamaal.f1@gmail.com>
2021-12-23 12:38:30 +0100

    Reading plist file in script

--- Scripts/reformat-names/main.go
+++ Scripts/reformat-names/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/xml"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("YES")
+	start := time.Now()
+
+	nameAvailabilityFile, err := os.Open("name_availability.plist")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer nameAvailabilityFile.Close()
+
+	scanner := bufio.NewScanner(nameAvailabilityFile)
+	scanner.Split(bufio.ScanLines)
+	var nameAvailabilityArray []string
+
+	for scanner.Scan() {
+		nameAvailabilityArray = append(nameAvailabilityArray, scanner.Text())
+	}
+
+	nameAvailabilityArray = nameAvailabilityArray[3 : len(nameAvailabilityArray)-1]
+	nameAvailabilityString := strings.Join(nameAvailabilityArray, "\n")
+	nameAvailabilityBytes := []byte(nameAvailabilityString)
+
+	var plistDict PlistDict
+	err = xml.Unmarshal(nameAvailabilityBytes, &plistDict)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(plistDict)
+
+	elapsed := time.Since(start)
+	fmt.Printf("done reformatting names in %s\n", elapsed)
+}
+
+type PlistDict struct {
+	XMLName xml.Name `xml:"dict"`
+	Keys    []string `xml:"key"`
 }
---
+++ Scripts/reformat-names/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  run.sh
+#  SFSViewer
+#
+#  Created by Kamaal M Farah on 23/12/2021.
+#  
+
+cd Scripts/reformat-names
+go run *.go
