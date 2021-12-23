8a2bbc0faba64a1621ae1be3bd744200404341ed
kamaal111 <kamaal.f1@gmail.com>
2021-12-23 17:53:55 +0100

    Reformatting names on every build

--- Scripts/reformat-names/run.sh
+++ Scripts/reformat-names/run.sh
@@ -6,5 +6,14 @@
 #  Created by Kamaal M Farah on 23/12/2021.
 #  
 
-cd Scripts/reformat-names
-go run *.go
+set -e
+
+GO_BIN=/usr/local/go/bin/go
+
+if ! which "$GO_BIN" > /dev/null; then
+    echo "error: go is not installed. Vistit https://go.dev to learn more."
+    exit 1
+else
+    cd Scripts/reformat-names
+    "$GO_BIN" run *.go
+fi
